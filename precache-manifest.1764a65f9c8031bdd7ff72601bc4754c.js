25f1628da623986207b4c0bb0269b79d083caabc
Andrijana <andrijana.r87@gmail.com>
2021-04-28 13:11:28 +0200

    Updates

---
+++ precache-manifest.1764a65f9c8031bdd7ff72601bc4754c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bba97ae11cd22fc23cb50515b8efda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc95e169f856394849a2",
+    "url": "/static/css/main.47a549d8.chunk.css"
+  },
+  {
+    "revision": "36fb66866aa0849e9ae2",
+    "url": "/static/js/2.cbd42ca1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.cbd42ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc95e169f856394849a2",
+    "url": "/static/js/main.3c669759.chunk.js"
+  },
+  {
+    "revision": "ef2dfd37c8e567995496",
+    "url": "/static/js/runtime-main.30e3f4a6.js"
+  },
+  {
+    "revision": "b4a46e3ea8d226ecb0dbd1db7cbae2f8",
+    "url": "/static/media/tayabak__5.b4a46e3e.jpg"
+  }
+]);
