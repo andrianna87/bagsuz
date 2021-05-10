e5482bb29e0570c09bc89bb3f1495cb848d13065
Andrijana <andrijana.r87@gmail.com>
2021-05-10 09:44:14 +0200

    Updates

---
+++ precache-manifest.97265ac495615014ccfb8eea8f5abf0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae512fa58ea9593a05058cf69f19195",
+    "url": "/bagsuz/index.html"
+  },
+  {
+    "revision": "db118fde77351e0c042c",
+    "url": "/bagsuz/static/css/main.47a549d8.chunk.css"
+  },
+  {
+    "revision": "36fb66866aa0849e9ae2",
+    "url": "/bagsuz/static/js/2.cbd42ca1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/bagsuz/static/js/2.cbd42ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db118fde77351e0c042c",
+    "url": "/bagsuz/static/js/main.6e36f6fb.chunk.js"
+  },
+  {
+    "revision": "9d600bf8e8bdfe897fd4",
+    "url": "/bagsuz/static/js/runtime-main.487e0d82.js"
+  },
+  {
+    "revision": "b4a46e3ea8d226ecb0dbd1db7cbae2f8",
+    "url": "/bagsuz/static/media/tayabak__5.b4a46e3e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1764a65f9c8031bdd7ff72601bc4754c.js"
+  "/bagsuz/precache-manifest.97265ac495615014ccfb8eea8f5abf0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bagsuz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
